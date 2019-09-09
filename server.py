c2067bb23c78a1d0f94070b6b15cba0705445fe7
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-09 00:52:59 -0400

    Inital Commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Hello World!'
+
+@app.route('/files/<path:path>')
+def send_file(path):
+    return send_from_directory('files', path)
+
+if __name__ == '__main__':
+    os.system("chrome http://localhost:5000")
+    app.run()
